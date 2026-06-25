ebbdf382931bf7ca55375edbe647fb63cf534530
Musaedi <zakhio2007@gmail.com>
2026-06-26 01:48:40 +0200

    Add args task

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    number = len(argv) - 1
+    if number == 0:
+        print("{} arguments.".format(number))
+    elif number == 1:
+        print("{} argument:".format(number))
+    else:
+        print("{} arguments:".format(number))
+    for i in range(1, number + 1):
+        print("{}: {}".format(i, argv[i]))
