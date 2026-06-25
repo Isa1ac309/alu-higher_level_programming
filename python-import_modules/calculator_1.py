a6d717f786289711e3bc0227aa6bcdcef7559303
Musaedi <zakhio2007@gmail.com>
2026-06-26 01:30:56 +0200

    Add calculator toolbox task

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function"""
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function"""
+    return (a * b)
+
+
+def div(a, b):
+    """My division function"""
+    return (a / b)
