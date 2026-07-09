be90b0ccc7b6169661b11888ffef4e2b243b3cb2
Musaedi <zakhio2007@gmail.com>
2026-07-09 15:00:16 +0200

    Add task 8: multiple returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sentence_length = len(sentence)
+    if sentence_length == 0:
+        first_character = None
+        return (sentence_length, first_character)
+    else:
+        first_character = sentence[0]
+        return (sentence_length, first_character)
+    
