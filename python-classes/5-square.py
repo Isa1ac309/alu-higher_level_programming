38a52e02ebb19245c64bd8e689cda335aa96ab73
Isa1ac309 <m.isaac@alustudent.com>
2026-07-24 14:39:21 +0200

    Add docstrings to 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+"""Defines a Square class with size validation and printing."""
 
 
 class Square:
+    """Represents a square."""
 
     def __init__(self, size=0):
+        """Initialize a new Square."""
         self.size = size
 
     @property
     def size(self):
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -19,9 +24,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return the current area of the square."""
         return self.__size ** 2
 
     def my_print(self):
+        """Print the square with the # character."""
         if self.__size == 0:
             print()
         else:
